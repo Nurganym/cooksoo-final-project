6b967054f0f17810d5830148cda833fc570f980f
Nurganym <nurganym14@gmail.com>
2020-05-04 01:01:50 +0300

    product list

--- my-app/src/components/ProductList.js
+++ my-app/src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
+import { ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
     state= {
@@ -15,7 +16,11 @@ export default class ProductList extends Component {
                   <div className="container">
                       <Title name="Наше" title="меню"/>
                       <div className="row">
-
+                        <ProductConsumer> 
+                          {(value) => {
+                          console.log(value)
+                          }}
+                         </ProductConsumer>  
                       </div>
                   </div>
               </div>
