ff73a6db1cef5014269637a3c4d6bd93179e1da1
Nurganym <nurganym14@gmail.com>
2020-05-07 02:46:53 +0300

    detail products

--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-import {ProductConsumer} from '../context'
+import {ProductConsumer} from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -10,7 +11,7 @@ export default class Product extends Component {
           <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3
           my-3">
               <div className="card">
-               <div className="img-conteiner p-5" onClick={ () => console.log('you clicked me on the image container')
+               <div className="img-container p-5" onClick={ () => console.log('you clicked me on the image container')
             }>
             <Link to="/details">
                 <img src={img} alt="товар" className="card-img-top" />
@@ -18,7 +19,7 @@ export default class Product extends Component {
             <button className="cart-btn" disabled={inCart ? true:false} onClick={()=> {console.log('added to the cart');
             }}
             >
-             {inCart?(<p className="text-capitalize mb-0" disabled> в корзину</p>):(<i className="fas fa-cart-plus" />)} 
+             {inCart?(<p className="text-capitalize mb-0" disabled> в корзину</p>):(<i class="fas fa-cart-plus"></i>)} 
             </button>
                </div>
                {/* {card footer} */}
@@ -35,11 +36,66 @@ export default class Product extends Component {
           </ProductWrapper>
       )
   }
+};
+
+Product.propTypes = {
+   product:PropTypes.shape({
+       id:PropTypes.number,
+       img:PropTypes.string,
+       title:PropTypes.string,
+       price:PropTypes.number,
+       inCart:PropTypes.bool,
+   }).isRequired
 }
 
 const ProductWrapper = styled.div `
 .card {
     border-color:transparent;
-    
+    transition: all 1s linear;
+    // background:orange;
+}
+.card-footer {
+    background: transparent;
+    border-top:transparent;
+    transition: all 1s linear;
+}
+&: hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2)
+    }
+    .card-footer{
+        background:rgba(247, 247, 247)
+    }
+}
+.img-container{
+    position:relative;
+    overflow:hidden;
+}
+.card-img-top{
+    transition: all 1s linear;
+}
+.img-container:hover .card-img-top{
+    transform:scale(1.2)
+}
+.cart-btn{
+    position:absolute;
+    bottom: 35px;
+    right: 0;
+    padding:0.2 rem 0.4rem;
+    // background:var(--lightBlue);
+    border:none;
+    // color:var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius:0.5rem 0 0 0;
+    // transform:translate(100%, 100%);
+    // transition: all 1s linear;
+}
+.img-container:hover .cart-btn{
+     transform:translate(0,0);
+}
+.cart-btn:hover{
+    // color:var(--mainBlue);
+    cursor:pointer;
 }
 `
--- my-app/src/context.js
+++ my-app/src/context.js
@@ -5,9 +5,22 @@ const ProductContext = React.createContext();
 //Consumer
 
  class ProductProvider extends Component {
-     state ={
-         products:storeProducts, 
+     state = {
+         products:[], 
          detailProduct: detailProduct
+     };
+     componentDidMount() {
+         this.setProducts();
+     }
+     setProducts = () =>{
+         let tempProducts = [];
+         storeProducts.forEach(item =>{
+             const singleItem = {...item};
+             tempProducts = [...tempProducts,singleItem]
+         })
+         this.setState(() => {
+             return {products:tempProducts}
+         })
      }
      handleDetail = () => {
          console.log('hello from detail')
